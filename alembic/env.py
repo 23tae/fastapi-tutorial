1af9a3cc61028de36d21a8b9e8c3ed19fc45e514
23tae <taehoonkim.dev@gmail.com>
2024-07-16 01:02:39 +0900

    Chore: alembic 변경

--- alembic/env.py
+++ alembic/env.py
@@ -1,12 +1,25 @@
+import os
+from dotenv import load_dotenv
 from logging.config import fileConfig
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 from alembic import context
 from app import models
 
+load_dotenv()
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
+# DATABASE_URL 생성
+db_url = (
+    f"postgresql://{os.getenv('DATABASE_USERNAME')}:{os.getenv('DATABASE_PASSWORD')}@"
+    f"{os.getenv('DATABASE_HOST')}/{os.getenv('DATABASE_NAME')}"
+)
+
+# sqlalchemy.url 설정
+config.set_main_option("sqlalchemy.url", db_url)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
