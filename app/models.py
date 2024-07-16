fca44edf2a4c44456fedea24b83c18de31db869b
23tae <taehoonkim.dev@gmail.com>
2024-07-17 00:26:01 +0900

    Chore: 배출량 클래스 추가

--- app/models.py
+++ app/models.py
@@ -1,4 +1,6 @@
-from sqlalchemy import Column, Integer, String, Boolean
+from datetime import datetime
+
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, Float
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
@@ -6,10 +8,24 @@ Base = declarative_base()
 
 class User(Base):
     __tablename__ = "users"
+
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String, unique=False, index=True)
-    email = Column(String, unique=True, index=True)
+    email = Column(String, unique=True, index=True, nullable=False)
     country = Column(String, unique=False, index=True)
     company = Column(String, unique=False, index=True)
     hashed_password = Column(String)
     is_active = Column(Boolean, default=True)
+    created_at = Column(DateTime, default=datetime.utcnow)
+
+
+class CarbonEmissions(Base):
+    __tablename__ = "carbon_emissions"
+
+    id = Column(Integer, primary_key=True, index=True)
+    country = Column(String, index=True, nullable=False)
+    timestamp = Column(DateTime, nullable=False)
+    emissions = Column(Float, nullable=False)
+    emissions_change = Column(Float, nullable=False)
+    emissions_per_capita = Column(Float, nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow)
