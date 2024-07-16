7bb9f7068e9e4ce0608dd41dc4eef473d2edc00d
23tae <taehoonkim.dev@gmail.com>
2024-07-16 22:22:50 +0900

    Chore: model 변경

--- app/models.py
+++ app/models.py
@@ -7,7 +7,9 @@ Base = declarative_base()
 class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True, index=True)
+    username = Column(String, unique=False, index=True)
     email = Column(String, unique=True, index=True)
+    country = Column(String, unique=False, index=True)
+    company = Column(String, unique=False, index=True)
     hashed_password = Column(String)
     is_active = Column(Boolean, default=True)
