e31b03ca2aad5f9048424c1c991a1ba744491ca7
23tae <taehoonkim.dev@gmail.com>
2024-07-18 17:03:58 +0900

    feat: 토큰 검증 기능

--- app/auth.py
+++ app/auth.py
@@ -3,12 +3,16 @@ from jose import JWTError, jwt
 import os
 from dotenv import load_dotenv
 from .schemas import TokenData
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
 
 load_dotenv()
 
 JWT_ACCESS_SECRET = os.getenv("JWT_ACCESS_SECRET")
 JWT_ALGORITHM = os.getenv("JWT_ALGORITHM")
 JWT_ACCESS_EXPIRE_MIN = int(os.getenv("JWT_ACCESS_EXPIRE_MIN"))
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 
 def create_access_token(data: dict, expires_delta: timedelta | None = None):
@@ -32,3 +36,12 @@ def verify_token(token: str, credentials_exception):
     except JWTError:
         raise credentials_exception
     return token_data
+
+
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    return verify_token(token, credentials_exception)
